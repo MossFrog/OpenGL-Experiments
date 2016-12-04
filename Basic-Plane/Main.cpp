e4b9aaed4b7ae3f1cc6b9bbc08340913343d2219
MossFrog <kaanozcelik@sabanciuniv.edu>
2016-12-04 19:10:29 +0200

    Added a simple plane example where the WASD Keys can be used for navigation.

---
+++ Basic-Plane/Main.cpp
@@ -0,0 +1,235 @@
+#include <iostream>
+
+#define GLEW_STATIC
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <SOIL\SOIL.h>
+
+#include "Shader.h"
+
+using namespace std;
+
+//-- Global Defínitions --//
+const GLint WIDTH = 800, HEIGHT = 600;
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+//-- The main key callback method that allows GLFW to receive and handle user input --//
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+int main()
+{
+	//-- Initial Definitions Section --//
+
+	//-- Initialization of the Vertex Buffers --//
+
+	//--------------------------------//
+
+	//- Initialize GLFW -//
+	glfwInit();
+
+	//-- Necessary Setups for GLFW --//
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+	//-- We allocate and create a pointer to the GLFW Window object --//
+	GLFWwindow * mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL Test", nullptr, nullptr);
+
+	int screenWidth, screenHeight;
+	glfwGetFramebufferSize(mainWindow, &screenWidth, &screenHeight);
+
+	if (nullptr == mainWindow)
+	{
+		cout << "GLFW Window failed to initialize!" << std::endl;
+		glfwTerminate();
+		return 1;
+	}
+
+	//-- Set the main window to the current context --//
+	glfwMakeContextCurrent(mainWindow);
+
+	//-- This set operation indicates that we are utilizing the modern shorthands for GLEW --//
+	glewExperimental = GL_TRUE;
+
+
+	//-- Initialize GLEW to setup the OpenGL Function pointers --//
+	if (GLEW_OK != glewInit())
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return 1;
+	}
+
+	//-- Viewport Dimension Setting --//
+	glViewport(0, 0, screenWidth, screenHeight);
+
+	//-- Designate the keyCallback Method --//
+	glfwSetKeyCallback(mainWindow, keyCallback);
+
+	//-- Enable Depth Testing --//
+	glEnable(GL_DEPTH_TEST);
+
+	//-- Building and Compiling the Shader Prorgam --//
+	Shader ourShader("./Shaders/vertex.vert", "./Shaders/fragment.frag");
+
+	//-- Initialization of Variables --//
+
+
+	//-- Vertex and Buffer Data --//
+
+	//-- Cube Vertex Array Definition --//
+	GLfloat vertices[] = {
+
+		-10.0f, -0.5f, -10.0f, 0.0f, 0.0f,
+		10.0f, -0.5f, -10.0f, 10.0f, 10.0f,
+		10.0f, -0.5f, 10.0f, 10.0f, 0.0f,
+		10.0f, -0.5f, 10.0f, 10.0f, 0.0f,
+		-10.0f, -0.5f, 10.0f, 0.0f, 0.0f,
+		-10.0f, -0.5f, -10.0f, 0.0f, 10.0f
+	};
+
+	//-- Not used yet --//
+	glm::vec3 cubePositions[] = {
+		glm::vec3(2.0f, 5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f, 3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f, 2.0f, -2.5f),
+		glm::vec3(1.5f, 0.2f, -1.5f),
+		glm::vec3(-1.3f, 1.0f, -1.5f)
+	};
+
+	GLuint VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//-- Position Attribute of the Vertices --//
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	//-- Texture Co-Ordinate Attribute of the Vertices --//
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
+
+	glBindVertexArray(0);
+
+
+
+
+
+	// ====================
+	// Texture 1
+	// ====================
+	GLuint texture1;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1); // All upcoming GL_TEXTURE_2D operations now have effect on our texture object
+	// Set our texture parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);	// Set texture wrapping to GL_REPEAT
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+	// Set texture filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// Load, create texture and generate mipmaps
+	int width, height;
+	unsigned char* image = SOIL_load_image("./grass.jpg", &width, &height, 0, SOIL_LOAD_RGB);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
+
+
+	//-- Main Program Loop --//
+	while (!glfwWindowShouldClose(mainWindow))
+	{
+		//-- Poll the window for incoming events such as keyboard input or mouse operations --//
+		glfwPollEvents();
+
+		//-- Render --//
+		//-- Clear the colorbuffer --//
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture1"), 0);
+
+		//-- Activating "ourShader" --//
+		ourShader.Use();
+
+		//-- Camera/View transformations --//
+		glm::mat4 view;
+		GLfloat radius = 10.0f;
+		GLfloat camX = sin(glfwGetTime()) * radius;
+		GLfloat camZ = cos(glfwGetTime()) * radius;
+		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+		//-- Projection --//
+		glm::mat4 projection;
+		projection = glm::perspective(45.0f, (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+		//-- Get the uniform locations --//
+		GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
+		GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
+		GLint projLoc = glGetUniformLocation(ourShader.Program, "projection");
+		//-- Pass the matrices to the shader --//
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		glBindVertexArray(VAO);
+
+
+		//-- Calculation of each individual model matrix --//
+		glm::mat4 model;
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+		//-- The angle changes over time, acceleration is caused by the sinusoidal change over time altering rotation. --//
+		GLfloat angle = 5.0f * sin(glfwGetTime());
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+		glBindVertexArray(0);
+
+		//-- Swap the screen buffers --//
+		glfwSwapBuffers(mainWindow);
+	}
+	//-- Terminate GLFW, clearing any resources allocated by GLFW. --//
+	glfwTerminate();
+
+	return 0;
+}
+
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
+	GLfloat cameraSpeed = 0.05f;
+	if (key == GLFW_KEY_W)
+		cameraPos += cameraSpeed * cameraFront;
+	if (key == GLFW_KEY_S)
+		cameraPos -= cameraSpeed * cameraFront;
+	if (key == GLFW_KEY_A)
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	if (key == GLFW_KEY_D)
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
